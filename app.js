cdc026288c616775246ac13f1b8ba32f5677d443
Mendler1994 <fernando.mendler@gmail.com>
2022-08-08 00:31:33 -0300

    localStorage

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ let carrito = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+    if(localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        pintarCarrito()
+    }
 })
+
 cards.addEventListener('click', e => {
     addCarrito(e)
 })
@@ -89,6 +94,8 @@ const pintarCarrito = () => {
     items.appendChild(fragment)
 
     pintarFooter()
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
 
 const pintarFooter = () => {
